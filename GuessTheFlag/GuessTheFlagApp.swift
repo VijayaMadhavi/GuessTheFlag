af5ba8a473d2134cca32c5ea5d95cc1310e92f3e
VijayaMadhavi <vijayamadhavi@hotmail.com>
2022-02-17 14:21:54 +0530

    add files

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Vijaya Madhavi on 08/02/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
